80e279e3c86bb6f3a59e2f75afb432606165caf4
Kirk Shillingford <tkshillinz@icloud.com>
2021-04-06 21:36:27 -0400

    added proper introduction

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,54 @@
-// Solving Domino problems with graph theory
-//
-// We can model our set of dominoes as a graph.
-// Specifically, the set of dominoes can be treated as an EdgeSet representation of a graph.
-//
-// To ask if a set of dominoes can be connected, can be rephrased as
-// "Does this graph have a Euler cycle?"
-//
-// A euler cycle is essentially a connected graph where you can start from one node and
-// pass through every other node back to the one you started with
-// without repeats or backtracking.
-//
-// How do we determine if a graph has a Euler's cycle?
-//
-// Well, there is theorem (Euler's Theorem):
-// "A Connected graph has an Euler cycle if and only if every vertex has even degree".
-//
-// So our solution now has two parts:
-//
-// check if every vertex (number) has an even degree (appears as a multiple of two)
-// check if the dominoes can be converted to a connected graph
+/* ---------------------------- INTRODUCTION --------------------------
+
+Solving Domino problems with graph theory
+
+Problem: Write a function `canChain` to determine whether a set of dominoes (an array of tuples) 
+can be chained.
+
+e.g. The dominoes (1, 2), (1, 3), (2, 3) can be chained together as (1, 2) (2, 3) (3, 1)
+So canChain([[1, 2], [1, 3], [2, 3]]) should return true.
+
+Conversely the dominoes (1, 2), (1, 3), (4, 4) cannot be chained because there is no domino
+that can connect to (4, 4).
+So canChain([[1, 2], [1, 3], [4, 4]])
+
+Some extra rules:
+- empty arrays should return true
+- the ends of the domino set should also match (making a perfect loop)
+
+Solution:
+
+We can model our set of dominoes as a graph where each domino represents two nodes (one for 
+each number on the domino) and the edge/line/arrow that connects them.
+
+When two dominoes have the same number, that means at least one of their nodes overlap 
+and they can be chained.
+
+Thus we can rephrase the problem of "Can the dominoes chain?" to
+"Do these nodes all connect?" and "Can we get from any node to every other node and back to 
+the start."
+
+It turns out, in graph theory, this type of configuration already has a name: A Euler Graph. 
+
+A Euler graph has a Euler cycle, which is exactly what we just described; a Euler cycle is a 
+path from one node on a graph that visits every other node and returns to the original graph 
+WITHOUT having to backtrack.
+
+So all we need to do to prove if our dominoes can chain is to prove that the graph those dominoes
+represent has a Euler cycle.
+
+And THAT it turns out, also has a formal definition.
+
+"A Connected graph has an Euler cycle if and only if every vertex has even degree".
+
+So our solution now has two parts:
+- check if every vertex (number) in our set of dominoes has an even degree 
+(appears as a multiple of two).
+
+- check if the dominoes can be converted to a Connected graph
+
+------------------------- DOMAIN -----------------------------------------
+*/
 
 type Dnum = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 
@@ -32,14 +62,16 @@ type AdjencyList = number[][]; // Representation as a list of connected nodes
 
 type EulerCondition = (ds: EdgeSet) => boolean; // Functions that test our Euler's theory
 
-// helper functions
+// -------------------------- HELPER FUNCTIONS --------------------------
+
 const id = (x: any) => x; // yes, this returns itself.
 
 // simplifies an edgeset down to its unique nodes by converting to and from a set
 type GetNodes = (_: EdgeSet) => number[];
 const getNodes: GetNodes = (dominoes) => [...new Set(dominoes.flatMap(id))];
 
-// conversion functions
+// ------------------------- CONVERSION FUNCTIONS -------------------
+
 type ToMatrix = (_: EdgeSet) => AdjencyMatrix;
 const toMatrix: ToMatrix = (dominoes) => {
   const nodes = getNodes(dominoes);
@@ -72,9 +104,13 @@ const toAdjencyList: ToAdjencyList = (graph) =>
         .map(([index, _]) => index) // keep indexes
   );
 
-// checks to see what nodes can be visited from other nodes.
-// updates the visited array every time it gets to a new one
-// if graph is connected, dfs should visit every node
+/* --------------------- IMPLEMENTATION ---------------------------------
+
+Our depthfirstsearch function hecks to see what nodes can be visited from other nodes.
+It updates the visited array every time it gets to a new node.
+If a graph is connected, dfs should visit every node.
+
+*/
 type DFS = (_: AdjencyList, __: boolean[], ___: number) => void;
 const depthFirstSearch: DFS = (graph, visited, node) => {
   visited[node] = true;
